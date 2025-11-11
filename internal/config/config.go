9a42c9e699164a969a94b6ba2e455d2339347b09
Gunārs Kunakovs <gunarskunakovs@gmail.com>
2025-11-11 13:25:52 +0200

    Add config validations (#20)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,33 +1,39 @@
 package config
 
-import "os"
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+
+	common "github.com/GunarsK-portfolio/portfolio-common/config"
+)
 
 type Config struct {
-	DBHost      string
-	DBPort      string
-	DBUser      string
-	DBPassword  string
-	DBName      string
-	Port        string
-	FilesAPIURL string
+	DBHost      string `validate:"required"`
+	DBPort      string `validate:"required,number,min=1,max=65535"`
+	DBUser      string `validate:"required"`
+	DBPassword  string `validate:"required"`
+	DBName      string `validate:"required"`
+	Port        string `validate:"required,number,min=1,max=65535"`
+	FilesAPIURL string `validate:"required,url"`
 }
 
 func Load() *Config {
-	return &Config{
-		DBHost:      getEnvRequired("DB_HOST"),
-		DBPort:      getEnvRequired("DB_PORT"),
-		DBUser:      getEnvRequired("DB_USER"),
-		DBPassword:  getEnvRequired("DB_PASSWORD"),
-		DBName:      getEnvRequired("DB_NAME"),
-		Port:        getEnvRequired("PORT"),
-		FilesAPIURL: getEnvRequired("FILES_API_URL"),
+	cfg := &Config{
+		DBHost:      common.GetEnvRequired("DB_HOST"),
+		DBPort:      common.GetEnvRequired("DB_PORT"),
+		DBUser:      common.GetEnvRequired("DB_USER"),
+		DBPassword:  common.GetEnvRequired("DB_PASSWORD"),
+		DBName:      common.GetEnvRequired("DB_NAME"),
+		Port:        common.GetEnvRequired("PORT"),
+		FilesAPIURL: common.GetEnvRequired("FILES_API_URL"),
 	}
-}
 
-func getEnvRequired(key string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		panic("Required environment variable " + key + " is not set")
+	// Validate configuration
+	validate := validator.New()
+	if err := validate.Struct(cfg); err != nil {
+		panic(fmt.Sprintf("Invalid configuration: %v", err))
 	}
-	return value
+
+	return cfg
 }
