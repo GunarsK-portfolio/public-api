598d6ef53925f985f4a3409bbfd12e7992ac7897
Gunārs Kunakovs <gunarskunakovs@gmail.com>
2025-11-13 13:20:09 +0200

    Add error wrapping to repository methods (#26)

--- internal/repository/work_experience.go
+++ internal/repository/work_experience.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/GunarsK-portfolio/public-api/internal/models"
 )
@@ -9,5 +10,8 @@ import (
 func (r *repository) GetAllWorkExperience(ctx context.Context) ([]models.WorkExperience, error) {
 	var experiences []models.WorkExperience
 	err := r.db.WithContext(ctx).Order("start_date DESC").Find(&experiences).Error
-	return experiences, err
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all work experience: %w", err)
+	}
+	return experiences, nil
 }
